1b3399b39f00a49af2806a92581f322748bd43dd
omgite333 <mgite48@gmail.com>
2026-01-30 19:12:08 +0530

    03: datbase setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ import {
   UserButton,
 } from '@clerk/nextjs'
 import { dark } from "@clerk/themes";
+import ConvexClientProvider from '@/components/providers'
 
 const inter = Inter({
   variable: "--font-inter",
@@ -36,35 +37,21 @@ export default function RootLayout({
 }>) {
   return (
 
-    <ClerkProvider  appearance={{
-      theme:dark,
-    }}>
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${plexMono.variable} antialiased`}
       >
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange >
-             <header className="flex justify-end items-center p-4 gap-4 h-16">
-                <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-             </header>
-              {children}
-              </ThemeProvider>
+        
+
+             <ConvexClientProvider>
+                {children}
+             </ConvexClientProvider>
+                
+             
+          
+              
       </body>
     </html>
-    </ClerkProvider>
+    
   );
 }
